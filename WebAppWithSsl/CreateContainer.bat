2a50d7767941af8a0ad59439141c8641d5c62f8a
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2023-02-04 16:45:36 +0200

    The ssl-certificate is generated as a part of a docker image preparation. It should be copied to the final image, BUT path is different: /root/.dotnet/corefx/cryptography/x509stores/my/ It doesn't work if the certificate is copied to /root/.aspnet/https

--- WebAppWithSsl/CreateContainer.bat
+++ WebAppWithSsl/CreateContainer.bat
@@ -1,10 +1,8 @@
 docker run ^
---name=WebAppWithSsl_2 ^
+--name=WebAppWithSsl_3 ^
 --publish 32770:80 ^
 --publish 32771:443 ^
---env Kestrel__Certificates__Development__Password=9ba2de68-55d7-4b61-b6c8-be06f10aa81f ^
 --env=ASPNETCORE_URLS=https://+:443;http://+:80 ^
---env=ASPNETCORE_ENVIRONMENT=Development ^
 ghcr.io/askrinnik/web-app-with-ssl
 
 pause
